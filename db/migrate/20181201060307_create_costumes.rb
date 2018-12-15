e37f35d18c680b10ba1a0bed6c4d635eb531258b
shikibum <satoshikibu@gmail.com>
2018-12-15 18:15:49 +0900

    カラムを追加

--- db/migrate/20181201060307_create_costumes.rb
+++ db/migrate/20181201060307_create_costumes.rb
@@ -1,7 +1,10 @@
+# frozen_string_literal: true
+
 class CreateCostumes < ActiveRecord::Migration[5.2]
   def change
     create_table :costumes do |t|
-      t.integer :objectID
+      t.integer :object_id
+      t.string :primary_image
 
       t.timestamps
     end
