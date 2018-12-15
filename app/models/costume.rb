f6f470ebdb3fcb5c6a1665fe25afa277c8294b53
shikibum <satoshikibu@gmail.com>
2018-12-15 18:16:27 +0900

    self.importメソッドを追加

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -1,4 +1,9 @@
+# frozen_string_literal: true
+
 class Costume < ApplicationRecord
-  def return_json(object_id)
+  def self.import(object_id)
+    response = CostumeApiClient.return_json(object_id)
+    costume = Costume.new(object_id: response['objectID'], primary_image: response['primaryImage'])
+    costume.save
   end
 end
--- app/models/costume_api_client.rb
+++ app/models/costume_api_client.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class CostumeApiClient
-  def return_json(object_id)
+  def self.return_json(object_id)
     response = Faraday.get "https://collectionapi.metmuseum.org/public/collection/v1/objects/#{object_id}"
     JSON.parse(response.body)
   end
