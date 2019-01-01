232c19f7fd68c030806c91a6742a66353878c6fa
Rina Mito <satoshikibu@gmail.com>
2019-01-01 18:09:13 +0900

    rubocop

--- app/controllers/costumes_controller.rb
+++ app/controllers/costumes_controller.rb
@@ -1,10 +1,9 @@
 # frozen_string_literal: true
 
 class CostumesController < ApplicationController
-  def  index
+  def index
     @costumes = Costume.all
   end
 
-  def show
-  end
+  def show; end
 end
