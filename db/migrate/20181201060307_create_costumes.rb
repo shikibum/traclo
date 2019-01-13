fd85af4810244674eee54c2c15ce6d826a2436f1
Rina Mito <satoshikibu@gmail.com>
2019-01-13 14:24:46 +0900

    日本語カラムを追加

--- db/migrate/20181201060307_create_costumes.rb
+++ db/migrate/20181201060307_create_costumes.rb
@@ -5,6 +5,15 @@ class CreateCostumes < ActiveRecord::Migration[5.2]
     create_table :costumes do |t|
       t.integer :object_id
       t.string :primary_image
+      t.string :title
+      t.string :title_japanese
+      t.string :culture
+      t.string :culture_japanese
+      t.string :country
+      t.string :country_japanese
+      t.string :region
+      t.string :region_japanese
+      t.string :object_url
 
       t.timestamps
     end
