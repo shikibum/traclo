3be04f8f2709808424933028e0f5996714d0c8ab
Rina Mito <satoshikibu@gmail.com>
2019-01-20 18:15:46 +0900

    continentごとの国名を表示

--- app/controllers/costumes_controller.rb
+++ app/controllers/costumes_controller.rb
@@ -2,10 +2,14 @@
 
 class CostumesController < ApplicationController
   def index
+    @costumes = Costume.all
     if params[:continent].present?
-      @costumes = Costume.where(continent: params[:continent])
-    else
-      @costumes = Costume.all
+      @costumes = @costumes.where(continent: params[:continent])
+    end
+    if params[:culture_or_country].present?
+      @costumes = @costumes
+                    .where(country_japanese: params[:culture_or_country])
+                    .or(Costume.where(culture_japanese: params[:culture_or_country]))
     end
   end
 
--- app/helpers/costumes_helper.rb
+++ app/helpers/costumes_helper.rb
@@ -1,4 +1,16 @@
 # frozen_string_literal: true
 
 module CostumesHelper
+  def extract_culture_and_country_from_costumes(continent)
+    Costume.where(continent: continent)
+    .select(:country_japanese, :culture_japanese)
+    .group(:country_japanese, :culture_japanese)
+    .map do |costume|
+      if costume.country_japanese.present?
+        costume.country_japanese
+      else
+        costume.culture_japanese
+      end
+    end.uniq.compact
+  end
 end
