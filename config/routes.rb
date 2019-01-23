a0e44e101c55fc787e2fb22150cf74c673fd75c2
Rina Mito <satoshikibu@gmail.com>
2019-01-23 14:44:12 +0900

    adminモデルを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :admins
   devise_for :users
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   resources :costumes
+
+  resources :admin_costumes
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
