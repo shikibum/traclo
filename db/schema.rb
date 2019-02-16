52c36b9b657b71bdb0c16a37c37b5b967ba93898
Rina Mito <satoshikibu@gmail.com>
2019-02-16 14:23:02 +0900

    pictureカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_12_132005) do
+ActiveRecord::Schema.define(version: 2019_02_15_142756) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2019_02_12_132005) do
     t.datetime "updated_at", null: false
     t.string "continent"
     t.string "image"
+    t.string "picture"
   end
 
   create_table "users", force: :cascade do |t|
